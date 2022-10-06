d14b54ec7716c417e10658dbafc2375e5a3d15e0
ffichtenmueller <frank.winkler@crayon.com>
2022-10-06 21:53:50 +0400

    implements CRUD for sample table

--- backend/src/handlers/articles/get-by-id.py
+++ backend/src/handlers/articles/get-by-id.py
@@ -2,23 +2,22 @@ import os
 import requests 
 import json
 import boto3
+from boto3.dynamodb.conditions import Key
 
 def getByIdHandler(event, context):
-    articleId = 
-    dynamodb = boto3.resource('dynamodb')
+    articleId = event["pathParameters"]["id"]
+    dynamodb = boto3.resource('dynamodb', endpoint_url="http://docker.for.mac.host.internal:8000")
     tablename = os.environ['SAMPLE_TABLE']
     table = dynamodb.Table(tablename)
     try:
 
-        response = table.get_item(
-            Key={
-                'id': articleId 
-            }
+        response = table.query(
+            KeyConditionExpression=Key('PersonKey').eq(articleId)
         ) 
 
         return {
             'statusCode': 200,
-            'body': f"{dynamodb.tables.all()}"
+            'body': json.dumps(response['Item'] )
             }
 
     except Exception as e:
